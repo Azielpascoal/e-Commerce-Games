6c94961f4fc58c770795a9e206cb9c73668b1e50
Aziel Pascoal <azielpascoalmuanzaantonio@gmail.com>
2021-08-25 10:54:26 +0100

    Estruturando pastas e pilhas de navegação

---
+++ src/stacks/mainStack.js
@@ -0,0 +1,12 @@
+import React from "react"
+import {createStackNavigator} from "@react-navigation/stack"
+
+
+const stack = createStackNavigator()
+export default()=>{
+    return(
+        <stack.Navigator>
+            
+        </stack.Navigator>
+    );
+}
