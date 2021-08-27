e39ac92eb8f5fba342de86fde003ff16873ec967
Aziel Pascoal <azielpascoalmuanzaantonio@gmail.com>
2021-08-27 09:46:44 +0100

    Estruturando paginas e configurando os componentes

---
+++ src/screens/home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import  {} from './style'
+import { View } from 'react-native'
+import Products from '../services/products.json'
+
+export default function Home(){
+
+    console.log(Products)
+
+    return(
+        <View>
+
+        </View>
+    );
+}
+
--- src/stacks/mainStack.js
+++ src/stacks/mainStack.js
@@ -1,12 +1,15 @@
-import React from "react"
-import {createStackNavigator} from "@react-navigation/stack"
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import HomePage from "../screens/home";
+import { StackActions } from "@react-navigation/native";
 
-
-const stack = createStackNavigator()
-export default()=>{
-    return(
-        <stack.Navigator>
-            
-        </stack.Navigator>
-    );
-}
+const stack = createStackNavigator();
+export default () => {
+  return (
+    <stack.Navigator
+        initialRouteName="Home"
+    >
+      <stack.Screen name="Home" component={HomePage} />
+    </stack.Navigator>
+  );
+};
