5f648469eea7cc01af22e5af4f3a36c43c90615f
Aziel Pascoal <azielpascoalmuanzaantonio@gmail.com>
2021-08-28 15:37:47 +0100

    Adicionando tela splah  na aplicação

---
+++ src/screens/Splash/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {} from './style'
+import {View} from 'react-native'
+
+
+export default function Splash(){
+    return(
+        <View>
+            
+        </View>
+    );
+}
