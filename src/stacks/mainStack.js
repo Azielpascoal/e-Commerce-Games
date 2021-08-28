4913451e02aa208074f0f82454839a5b871ce3f0
Aziel Pascoal <azielpascoalmuanzaantonio@gmail.com>
2021-08-28 15:47:48 +0100

    Adicionando rota splash na aplicação

--- src/stacks/mainStack.js
+++ src/stacks/mainStack.js
@@ -1,18 +1,20 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import HomePage from "../screens/Home";
+import SplashPage from "../screens/Splash"
 import { StackActions } from "@react-navigation/native";
 
 const stack = createStackNavigator();
 export default () => {
   return (
     <stack.Navigator
-      initialRouteName="Home"
+      initialRouteName="Splash"
       screenOptions={{
         headerShown: false,
       }}
     >
       <stack.Screen name="Home" component={HomePage} />
+      <stack.Screen name="Splash" component={SplashPage} />
     </stack.Navigator>
   );
 };
