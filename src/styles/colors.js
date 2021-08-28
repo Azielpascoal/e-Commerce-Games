2d3d3d9caed6c94e767a6771cdfb153fb832e50e
Aziel Pascoal <azielpascoalmuanzaantonio@gmail.com>
2021-08-28 15:44:22 +0100

    Configurando cores da aplicação

---
+++ src/styles/colors.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { StyleSheet } from "react-native";
+
+export const colors = StyleSheet.create({
+  Black: {
+    color: "#1A2A3A",
+  },
+  Yellow: {
+    color: "#F7D86D",
+  },
+  White: {
+    color: "#FFFFFF",
+  },
+});
+export const backColor = StyleSheet.create({
+  Black: {
+    backgroundColor: "#1A2A3A",
+  },
+  Yellow: {
+    backgroundColor: "#F7D86D",
+  },
+  White: {
+    colbackgroundColoror: "#FFFFFF",
+  },
+});
