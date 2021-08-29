0b3d241bd8e82271622ce7aafd2f5c0f9188c68e
Aziel Pascoal <azielpascoalmuanzaantonio@gmail.com>
2021-08-29 22:50:07 +0100

    Estruturando o flastlist

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -12,28 +12,41 @@ import {
   CartImage,
 } from "./style";
 import Products from "../../services/products.json";
+import { FlatList } from "react-native";
 
 export default function Home() {
-  const [products, setProducts] = useState([]);
-//   useEffect(() => {
-//     const AllProducts = Products.map((product) => {
-//       return product;
-//     });
-//     setProducts(AllProducts);
-//     console.log("Data", AllProducts)
-//   }, [products]);
+  // const [products, setProducts] = useState([]);
+
+  //   useEffect(() => {
+  //     const AllProducts = Products.map((product) => {
+  //       return product;
+  //     });
+  //     setProducts(AllProducts);
+  //     console.log("Data", AllProducts)
+  //   }, [products]);
+  const products = Products.map((product) => {
+    return product;
+  });
 
   return (
     <Container>
       <Top>
         <InfoText>Bem vindo ao e-commerce games</InfoText>
       </Top>
-      <ProductArea>
-        {Products.map((index, key) => {
-           
+      <FlatList
+        data={products}
+        contentContainerStyle={{
+          width: "100%",
+          flex: 1,
+          borderTopRightRadius: 35,
+          marginTop: -60,
+          backgroundColor: "#fff",
+          padding: "4%",
+        }}
+        renderItem={({ index }) => {
           return (
-            <ProductCard key={key} >
-              <ProductImage source={{uri:index.image}}/>
+            <ProductCard key={key}>
+              <ProductImage source={{ uri: index.image }} />
               <ProductInfoArea>
                 <ProductInfoText>Nome:{index.name}</ProductInfoText>
                 <ProductInfoText>Preço:${index.price}</ProductInfoText>
@@ -41,8 +54,9 @@ export default function Home() {
               </ProductInfoArea>
             </ProductCard>
           );
-        })} 
-      </ProductArea>
+        }}
+        keyExtractor={(index) => index.id}
+      ></FlatList>
     </Container>
   );
 }
