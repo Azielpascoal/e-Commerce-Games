6560f860ab197279e56ae23f02b1a0dfb4b22fb0
Aziel Pascoal <azielpascoalmuanzaantonio@gmail.com>
2021-08-29 23:27:05 +0100

    Adicionando o main tab ao main stack

--- src/stacks/mainStack.js
+++ src/stacks/mainStack.js
@@ -1,20 +1,20 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack"
 import SplashPage from "../screens/Splash"
-import HomePage from "../screens/Home"
+import MainTab from "./mainTab";
 
 
 const stack = createStackNavigator();
 export default () => {
   return (
     <stack.Navigator
-      initialRouteName="Home"
+      initialRouteName="MainTab"
       screenOptions={{
         headerShown: false,
       }}
     >
       <stack.Screen name="Splash" component={SplashPage} />
-      <stack.Screen name="Home" component={HomePage}/>
+      <stack.Screen name="MainTab" component={MainTab}/>
     </stack.Navigator>
   );
 };
