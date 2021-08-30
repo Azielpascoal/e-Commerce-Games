ffa3b59ae4e82db0f14392f63536b724781d72c3
Aziel Pascoal <azielpascoalmuanzaantonio@gmail.com>
2021-08-30 11:34:27 +0100

    Configurando o tab com estilos

--- src/stacks/mainTab.js
+++ src/stacks/mainTab.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { StyleSheet } from "react-native";
+import { Image, StyleSheet, View, Text } from "react-native";
 import HomePage from "../screens/Home";
 import CartPage from "../screens/Cart";
 import { useCart } from "../context/cart";
-import { View, Text } from "react-native";
-import cartProvider from "../context/cart";
+import HomeIcon from "../assets/home.png";
+import CartIcon from "../assets/shopping-cart.png";
 
 const tab = createBottomTabNavigator();
 export default () => {
@@ -20,10 +20,20 @@ export default () => {
           height: 70,
           ...style.shadow,
           alignItems: "center",
+          marginRight: 15,
+          marginLeft: 15,
+          bottom: 5,
+          alignSelf: "center",
+          borderRadius: 18,
+          backgroundColor: "#fff",
         },
       }}
     >
-      <tab.Screen name="Home" component={HomePage} />
+      <tab.Screen
+        name="Home"
+        component={HomePage}
+        options={{ tabBarIcon: homeIcon }}
+      />
       <tab.Screen
         name="Carrinho"
         component={CartPage}
@@ -32,14 +42,51 @@ export default () => {
     </tab.Navigator>
   );
 };
-export const iconCart=() =>{
+export const iconCart = ({focused}) => {
   const { cart } = useCart();
   return (
-    <View>
-      {/* <Text>{Object.keys(cart.length)}</Text> */}
+    <View
+      style={{
+        width: 55,
+        height: 55,
+        alignItems: "center",
+        justifyContent: "center",
+        alignSelf: "center",
+        marginRight: 15,
+        flexDirection:'row',
+        backgroundColor:focused ? "#f7d86d" : "#fff",
+        borderRadius:12
+      }}
+    >
+      <Image
+        source={CartIcon}
+        style={{ width: 35, height: 35, tintColor: "#000",alignSelf:'center',marginLeft:12 }}
+      />
+      <Text style={{backgroundColor:'#f00',borderRadius:18,marginBottom:15,marginRight:14}}>{Object.keys(cart.length)}</Text>
     </View>
   );
-}
+};
+export const homeIcon = ({focused}) => {
+  return (
+    <View
+      style={{
+        width: 55,
+        height: 55,
+        alignItems: "center",
+        justifyContent: "center",
+        alignSelf: "center",
+        marginRight: 15,
+        backgroundColor:focused ? "#f7d86d" : "#fff",
+        borderRadius:12
+      }}
+    >
+      <Image
+        source={HomeIcon}
+        style={{ width: 35, height: 35, tintColor: "#000" }}
+      />
+    </View>
+  );
+};
 const style = StyleSheet.create({
   shadow: {
     shadowColor: "#000000",
@@ -52,3 +99,4 @@ const style = StyleSheet.create({
     elevation: 12,
   },
 });
+// {Object.keys(cart.length)}
