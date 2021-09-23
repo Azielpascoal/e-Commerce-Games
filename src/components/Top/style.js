c7c56f4330483e7ebcf7853e298c3d67e000a9f3
Aziel Pascoal <azielpascoalmuanzaantonio@gmail.com>
2021-09-24 00:21:24 +0100

    add: adicionando estilo no novo componente

---
+++ src/components/Top/style.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components/native'
+
+
+export const Container = styled.SafeAreaView`
+    width: 100%;
+    height: 24px;
+  
+   
+`;
+export const BackG = styled.ImageBackground`
+    width: 414px;
+    height: 100%;
+`;
