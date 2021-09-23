16e60682ed4ae5c643e5f229d2ec96732d37420c
Aziel Pascoal <azielpascoalmuanzaantonio@gmail.com>
2021-09-24 00:21:00 +0100

    add:novo componete para header

---
+++ src/components/Top/headertop.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Container,BackG} from './style'
+import Top from '../../assets/splash1.png'
+
+export default()=>{
+    return(
+        <Container>
+            <BackG
+                source={Top}
+            />
+        </Container>
+    );
+}
